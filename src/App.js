600eaea0ba1fcf26e3c0d4aee4e1c65239bc0eb3
cvasquez32 <cvasquez0100@gmail.com>
2019-09-04 18:45:41 -0500

    Created Posts component to pass in the data to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import Posts from './Components/Posts';
 import axios from 'axios';
 import './App.css';
 
@@ -20,8 +21,9 @@ const App = () => {
   }, []);
 
   return(
-    <div className="container">
-      <h1>My App</h1>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={posts} loading={loading}/>
     </div>
   );
 };
