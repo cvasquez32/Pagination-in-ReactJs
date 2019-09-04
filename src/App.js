7e8f3ac753488bc5acbfde51e91984dd7162ae7d
cvasquez32 <cvasquez0100@gmail.com>
2019-09-04 14:19:43 -0500

    Adding tags, and class tags

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import styles from './App.module.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+        
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th>S/N</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr>
+            <td>1</td>
+            <td>Abel</td>
+            <td>Agoi</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Muyiwa</td>
+            <td>Aregbesola</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Opeyemi</td>
+            <td>Agoi</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Ope</td>
+            <td>Aina</td>
+          </tr>
+      </tbody>
+    </table>
+
+
+    <div className={styles.pagination}>
+      <span>&laquo;</span>
+      <span className={styles.active}>1</span>
+      <span>2</span>
+      <span>3</span>
+      <span>4</span>
     </div>
+
+  </div>
   );
 }
 
