53b5d5e883458f27917e3a452c715300875a089d
cvasquez32 <cvasquez0100@gmail.com>
2019-09-04 19:37:27 -0500

    Changed the amount of posts we receive from 100 to 10

--- src/App.js
+++ src/App.js
@@ -20,10 +20,16 @@ const App = () => {
     fetchPosts();
   }, []);
 
+  // Get current Posts
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+
   return(
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
-      <Posts posts={posts} loading={loading}/>
+      <Posts posts={currentPosts} loading={loading}/>
     </div>
   );
 };
